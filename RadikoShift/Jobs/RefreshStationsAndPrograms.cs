f0f28f5ba42f34e27aa208bd2e6b33433df23903
noobow34 <skywind.hk34@gmail.com>
2026-02-06 20:01:39 +0900

    コンソールログ出力

--- RadikoShift/Jobs/RefreshStationsAndPrograms.cs
+++ RadikoShift/Jobs/RefreshStationsAndPrograms.cs
@@ -8,10 +8,13 @@ namespace RadikoShift.Jobs
     {
         public Task Execute(IJobExecutionContext context)
         {
+            this.JournalWriteLine("番組表更新開始");
+
             string radikoMail = Environment.GetEnvironmentVariable("RADIKO_MAIL") ?? "";
             string radikoPass = Environment.GetEnvironmentVariable("RADIKO_PASS") ?? "";
             Radiko.Login(radikoMail, radikoPass).Wait();
 
+            this.JournalWriteLine("放送局取得");
             var stations = Radiko.GetStations(true).Result;
             ShiftContext sContext = new();
             var existStations =  sContext.Stations.Where(s => (stations.Select(s => s.Id).ToArray().Contains(s.Id)));
@@ -19,19 +22,15 @@ namespace RadikoShift.Jobs
             sContext.Stations.AddRange(stations);
             foreach (var station in stations)
             {
-                try
-                {
-                    var programs = Radiko.GetPrograms(station).Result;
-                    var existPrograms = sContext.Programs.Where(p => (programs.Select(p => p.Id).ToArray().Contains(p.Id)));
-                    sContext.RemoveRange(existPrograms);
-                    sContext.Programs.AddRange(programs);
-                }
-                catch (Exception e)
-                {
-                    
-                }
+                this.JournalWriteLine(station.Name!);
+                var programs = Radiko.GetPrograms(station).Result;
+                var existPrograms = sContext.Programs.Where(p => (programs.Select(p => p.Id).ToArray().Contains(p.Id)));
+                sContext.RemoveRange(existPrograms);
+                sContext.Programs.AddRange(programs);
             }
+            this.JournalWriteLine("保存");
             sContext.SaveChanges();
+            this.JournalWriteLine("番組表更新終了");
 
             return Task.CompletedTask;
         }
---
+++ RadikoShift/ObjectExtension.cs
@@ -0,0 +1,19 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace RadikoShift
+{
+    public static class ObjectExtension
+    {
+        private static readonly ConditionalWeakTable<object, string> _instanceIdList = [];
+
+        public static void JournalWriteLine(this object obj, string value)
+        {
+            if (!_instanceIdList.TryGetValue(obj, out string? instanceId))
+            {
+                instanceId = Ulid.NewUlid().ToString();
+                _instanceIdList.Add(obj, instanceId);
+            }
+            Console.WriteLine($"【{obj.GetType().Name}:{instanceId}】{value}");
+        }
+    }
+}
