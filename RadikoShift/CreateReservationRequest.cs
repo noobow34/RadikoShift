bee6999134caf8c37577c7a76f7d59f7871db04a
noobow34 <skywind.hk34@gmail.com>
2026-02-18 08:37:10 +0900

    予約内容編集 実装中

--- RadikoShift/CreateReservationRequest.cs
+++ RadikoShift/CreateReservationRequest.cs
@@ -3,6 +3,17 @@
     public class CreateReservationRequest
     {
         public string ProgramId { get; set; } = null!;
+
+        public string Title { get; set; } = null!;
+
+        public string? CastName { get; set; }
+
+        public TimeOnly StartTime { get; set; }
+
+        public TimeOnly EndTime { get; set; }
+
         public RepeatType RepeatType { get; set; }
+
+        public bool IsEdited { get; set; }
     }
 }
