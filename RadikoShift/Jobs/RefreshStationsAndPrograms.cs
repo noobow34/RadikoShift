b97e7d5ca78a613e2428f753ad5596a20bbf0c4f
noobow34 <skywind.hk34@gmail.com>
2026-02-24 00:25:07 +0900

    エラー対応

--- RadikoShift/Jobs/RefreshStationsAndPrograms.cs
+++ RadikoShift/Jobs/RefreshStationsAndPrograms.cs
@@ -9,39 +9,46 @@ namespace RadikoShift.Jobs
     {
         public Task Execute(IJobExecutionContext context)
         {
-            this.JournalWriteLine("番組表更新開始");
-
-            string radikoMail = Environment.GetEnvironmentVariable("RADIKO_MAIL") ?? "";
-            string radikoPass = Environment.GetEnvironmentVariable("RADIKO_PASS") ?? "";
-            Radiko.Login(radikoMail, radikoPass).Wait();
-
-            this.JournalWriteLine("放送局取得");
-            var stations = Radiko.GetStations(true).Result;
-            ShiftContext sContext = new();
-            var areas = sContext.Areas.ToDictionary(a => a.AreaCode , a => a.AreaName);
-            foreach (var station in stations)
+            try
             {
-                if (areas.TryGetValue(station.Area!, out string? value))
+                this.JournalWriteLine("番組表更新開始");
+
+                string radikoMail = Environment.GetEnvironmentVariable("RADIKO_MAIL") ?? "";
+                string radikoPass = Environment.GetEnvironmentVariable("RADIKO_PASS") ?? "";
+                Radiko.Login(radikoMail, radikoPass).Wait();
+
+                this.JournalWriteLine("放送局取得");
+                var stations = Radiko.GetStations(true).Result;
+                ShiftContext sContext = new();
+                var areas = sContext.Areas.ToDictionary(a => a.AreaCode, a => a.AreaName);
+                foreach (var station in stations)
                 {
-                    station.AreaName = value;
+                    if (areas.TryGetValue(station.Area!, out string? value))
+                    {
+                        station.AreaName = value;
+                    }
                 }
+                var tran = sContext.Database.BeginTransaction();
+                this.JournalWriteLine("stations全件削除");
+                sContext.Database.ExecuteSqlRaw("DELETE FROM stations");
+                this.JournalWriteLine("programs全件削除");
+                sContext.Database.ExecuteSqlRaw("DELETE FROM programs");
+                sContext.Stations.AddRange(stations);
+                foreach (var station in stations)
+                {
+                    this.JournalWriteLine(station.Name!);
+                    var programs = Radiko.GetPrograms(station).Result;
+                    sContext.Programs.AddRange(programs);
+                }
+                this.JournalWriteLine("保存");
+                sContext.SaveChanges();
+                tran.Commit();
+                this.JournalWriteLine("番組表更新終了");
             }
-            var tran = sContext.Database.BeginTransaction();
-            this.JournalWriteLine("stations全件削除");
-            sContext.Database.ExecuteSqlRaw("DELETE FROM stations");
-            this.JournalWriteLine("programs全件削除");
-            sContext.Database.ExecuteSqlRaw("DELETE FROM programs");
-            sContext.Stations.AddRange(stations);
-            foreach (var station in stations)
+            catch (Exception ex)
             {
-                this.JournalWriteLine(station.Name!);
-                var programs = Radiko.GetPrograms(station).Result;
-                sContext.Programs.AddRange(programs);
+                this.JournalWriteLine($"放送局・番組表更新ジョブ実行中に例外が発生:{ex.ToString}");
             }
-            this.JournalWriteLine("保存");
-            sContext.SaveChanges();
-            tran.Commit();
-            this.JournalWriteLine("番組表更新終了");
 
             return Task.CompletedTask;
         }
